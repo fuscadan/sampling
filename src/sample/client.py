317a05124af652cd45fab7d0bc4d96ced0284e77
Dan Fusca <danfusca@Dans-MacBook-Pro.local>
2023-02-10 01:58:35 -0500

    app mockup

---
+++ src/sample/client.py
@@ -0,0 +1,14 @@
+import click
+
+from sample.infer import mock_infer
+
+
+@click.group()
+def cli() -> None:
+    pass
+
+
+@cli.command()
+@click.option("--config", type=str, required=True)
+def infer(config: str) -> None:
+    mock_infer(config=config)
---
+++ src/sample/infer.py
@@ -0,0 +1,14 @@
+import os
+
+import yaml
+
+from constants import DIRECTORY_CONFIGS
+
+
+def mock_infer(config: str) -> None:
+    """placeholder for the main function called by the client"""
+    file_path = os.path.join(DIRECTORY_CONFIGS, f"{config}.yaml")
+    with open(file_path) as f:
+        config_dict = yaml.safe_load(f)
+
+    print(config_dict)
