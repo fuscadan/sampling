62997237499d4d603b502939daf2bb3e9d4a99a3
Dan Fusca <danfusca@Dans-MacBook-Pro.local>
2023-02-20 00:50:02 -0500

    distributions now have .leaves method

--- src/gfs/sample/distributions.py
+++ src/gfs/sample/distributions.py
@@ -5,8 +5,11 @@ from gfs.sample.tree import Leaf, Side, Tree
 
 class Distribution(ABC):
     @abstractmethod
-    def tree(self) -> Tree:
+    def leaves(self) -> list[Leaf]:
         pass
+
+    def tree(self) -> Tree:
+        return Tree(self.leaves())
 
 
 class Uniform(Distribution):
@@ -14,12 +17,11 @@ class Uniform(Distribution):
         super().__init__()
         self.bit_depth_domain = bit_depth_domain
 
-    def tree(self) -> Tree:
+    def leaves(self) -> list[Leaf]:
         leaf = Leaf(
             multiplicity=0, sides=[Side(endpoint=0, bit_depth=self.bit_depth_domain)]
         )
-
-        return Tree([leaf])
+        return [leaf]
 
 
 class Linear(Distribution):
@@ -28,7 +30,7 @@ class Linear(Distribution):
         self.bit_depth_domain = bit_depth_domain
         self.reverse_int = 0 if reverse else 1
 
-    def tree(self) -> Tree:
+    def leaves(self) -> list[Leaf]:
         leaves: list[Leaf] = list()
         for j in range(0, self.bit_depth_domain):
             for i in range(0, 2 ** (self.bit_depth_domain - j - 1)):
@@ -37,4 +39,4 @@ class Linear(Distribution):
                     Leaf(multiplicity=j, sides=[Side(endpoint=endpoint, bit_depth=j)])
                 )
 
-        return Tree(leaves)
+        return leaves
