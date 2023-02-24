6f797dce5505071f78de1f2b257431ba7f7d2d44
Dan Fusca <danfusca@Dans-MacBook-Pro.local>
2023-02-23 22:02:28 -0500

    correcting randint util function (unused)

--- src/gfs/sample/utils.py
+++ src/gfs/sample/utils.py
@@ -5,5 +5,5 @@ def randint(n_bits) -> int:
     """
     slower but possibly better random number generation compared to random.randrange
     """
-    n_bytes = (n_bits // 9) + 1
-    return int.from_bytes(urandom(n_bytes)) >> 3
+    n_bytes = (n_bits // 8) + ((n_bits % 8) + 7) // 8
+    return int.from_bytes(urandom(n_bytes)) >> (8 * n_bytes - n_bits)
