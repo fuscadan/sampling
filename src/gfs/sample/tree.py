99529fc03375ca13aa081c30de16818ad42b46a5
Dan Fusca <danfusca@Dans-MacBook-Pro.local>
2023-03-08 18:04:17 -0500

    fixing Histogram class

--- src/gfs/sample/tree.py
+++ src/gfs/sample/tree.py
@@ -1,6 +1,5 @@
 from random import randrange
 
-from gfs.sample.histogram import Histogram
 from gfs.sample.leaf import Label, Leaf, LeafList
 
 SAMPLING_MAX_RETRIES = 100000
@@ -8,8 +7,13 @@ SAMPLING_MAX_RETRIES = 100000
 
 class Tree:
     def __init__(self, leaves: LeafList) -> None:
-        self._depth: int = self._compute_required_depth(leaves)
+        self._n_blocks: int = self._compute_n_blocks(leaves)
+        self._depth: int = self._compute_required_depth()
         self._leaves_labeled: dict[Label, Leaf] = self._label_leaves(leaves)
+
+    @property
+    def n_blocks(self) -> int:
+        return self._n_blocks
 
     @property
     def depth(self) -> int:
@@ -19,9 +23,11 @@ class Tree:
     def leaves_labeled(self) -> dict[Label, Leaf]:
         return self._leaves_labeled
 
-    def _compute_required_depth(self, leaves: LeafList) -> int:
-        n_total_blocks = sum([leaf.n_blocks for leaf in leaves])
-        return n_total_blocks.bit_length()
+    def _compute_n_blocks(self, leaves: LeafList) -> int:
+        return sum([leaf.n_blocks for leaf in leaves])
+
+    def _compute_required_depth(self) -> int:
+        return self.n_blocks.bit_length()
 
     def _label_leaves(self, leaves: LeafList) -> dict[Label, Leaf]:
         leaves.sort(key=lambda leaf: leaf.bit_depth, reverse=True)
@@ -39,7 +45,7 @@ class Tree:
 
         return leaves_labeled
 
-    def _sample_once(self) -> tuple[int, ...]:
+    def sample_once(self) -> tuple[int, ...]:
         for n in range(0, SAMPLING_MAX_RETRIES):
             label: Label = Label(randrange(1 << self.depth), self.depth)
 
@@ -52,15 +58,4 @@ class Tree:
         raise Exception("Maximum sampling retries exceeded.")
 
     def sample(self, n_samples: int) -> list[tuple[int, ...]]:
-        return [self._sample_once() for i in range(0, n_samples)]
-
-    def histogram(self, n_samples: int) -> Histogram:
-        histogram: dict[tuple[int, ...], int] = dict()
-        for i in range(0, n_samples):
-            sample = self._sample_once()
-            if isinstance(histogram.get(sample), int):
-                histogram[sample] += 1
-            else:
-                histogram[sample] = 1
-
-        return Histogram(histogram)
+        return [self.sample_once() for i in range(0, n_samples)]
