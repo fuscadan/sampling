73fe37633c49b7230625268b3f5f309ce1603140
Dan Fusca <danfusca@Dans-MacBook-Pro.local>
2023-03-31 00:12:42 -0400

    adding elements.py and necessary refactoring

--- src/gfs/constants.py
+++ src/gfs/constants.py
@@ -1,1 +1,3 @@
+VERSION = "0.1.0"
+
 DIRECTORY_PROJECT_ROOT = "src/gfs/projects"
---
+++ src/gfs/sample/elements.py
@@ -0,0 +1,55 @@
+import logging
+from typing import Iterable, NamedTuple
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+class DataPoint(NamedTuple):
+    id: int
+    value: tuple[int, ...]
+
+
+class Sample(tuple[float, ...]):
+    def __init__(self, iterable: Iterable):
+        if len(self) == 0:
+            raise ValueError("Sample cannot be empty.")
+
+
+class Distribution(tuple[float, ...]):
+    def __init__(self, iterable: Iterable):
+        if round(sum(self), 6) != 1:
+            raise ValueError("Distribution must sum to 1.")
+
+
+class Histogram(dict[Sample, int]):
+    def __missing__(self, key):
+        return 0
+
+
+class PosteriorSamples(list[Sample]):
+    def __init__(self, iterable: Iterable, axes: list[str]) -> None:
+        list.__init__(self, iterable)
+        self.axes = axes
+
+    @property
+    def histogram(self) -> Histogram:
+        hist = Histogram()
+        for sample in self:
+            hist[sample] += 1
+        return hist
+
+
+class PredictiveDists(list[Distribution]):
+    def __init__(self, iterable: Iterable, categories: list[str]) -> None:
+        list.__init__(self, iterable)
+        self.categories = categories
+
+    @property
+    def mean(self) -> Distribution:
+        cat_means: list[float] = list()
+        n_dists = len(self)
+        for i in range(len(self.categories)):
+            cat_means.append(sum([dist[i] for dist in self]) / n_dists)
+
+        return Distribution(cat_means)
