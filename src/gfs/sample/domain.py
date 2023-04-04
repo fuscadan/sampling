676ce51fe04ab4cf1598568c80ba515a0e385f2a
Dan Fusca <danfusca@Dans-MacBook-Pro.local>
2023-04-03 23:51:29 -0400

    moving scale functionality to Domain class

--- src/gfs/sample/domain.py
+++ src/gfs/sample/domain.py
@@ -2,11 +2,37 @@ from typing import NamedTuple
 
 
 class Axis(NamedTuple):
-    id: int
+    name: str
     left_endpoint: float
     right_endpoint: float
     bit_depth: int
 
 
 class Domain(list[Axis]):
-    pass
+    @property
+    def bit_depth(self):
+        return sum([axis.bit_depth for axis in self])
+
+    @staticmethod
+    def _get_scale(axis: Axis) -> float:
+        a = axis.left_endpoint
+        b = axis.right_endpoint
+        return (b - a) / (1 << axis.bit_depth)
+
+    def _rescale(self, axis: Axis, coordinate: int) -> float:
+        a = axis.left_endpoint
+        scale = self._get_scale(axis)
+        return a + coordinate * scale
+
+    def scale(self, int_coords: tuple[int, ...]) -> tuple[float, ...]:
+        if len(self) != len(int_coords):
+            raise ValueError(
+                "Number of integer coordinates not equal to dimension of Domain."
+            )
+
+        float_coords = [
+            self._rescale(axis, coordinate)
+            for axis, coordinate in zip(self, int_coords)
+        ]
+
+        return tuple(float_coords)
